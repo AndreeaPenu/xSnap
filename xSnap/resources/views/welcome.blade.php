196aea60239ffa2691dea20a5978dde69fb82c4c
andreea penu <andreea_penu@hotmail.com>
2017-11-27 19:45:00 +0100

    laravel + vue install

---
+++ xSnap/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Vue JS</title>
+
+    
+    </head>
+    <body>
+        <div id="app">
+            <p>@{{ message }}</p>
+        </div>
+
+      <script src="js/vue.min.js"></script>
+      <script src="js/script.js"></script>
+        
+    </body>
+</html>
